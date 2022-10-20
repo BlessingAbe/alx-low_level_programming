8adef1ae906af2783dcf2e3c255e671ecc81dc05
BlessingAbe <blessingeloghene@gmail.com>
2022-10-20 12:32:31 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - function with one integer argument
+ * @n: 1st int type argument
+ * Description: draw a diagonal line on terminal with loops
+ * Return: na
+ */
+void print_diagonal(int n)
+{
+int i, j;
+	i = 0;
+	while (n > 0)
+	{
+		j = i;
+		while (j > 0)
+		{
+		_putchar(' ');
+		j--;
+	}
+	_putchar('\\');
+	_putchar('\n');
+	i++;
+	n--;
+	}
+	if (i < 1)
+		_putchar('\n');
+}
